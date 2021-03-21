a54bfeaf348bff99eef031bb6e6508f1a474f72b
root <phlip.marcotte@gmail.com>
2021-03-21 18:38:20 +0000

    executable scripts and fixed backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,30 @@
-backupPath=/home/steam/backups
 echo "Starting backup"
-printf '2\n1\ny\n0\n0\n' | ./menu.sh
+backupPath=/home/steam/backups
+#!/bin/bash
+
+## Get the current date as variable.
+TODAY="$(date +%Y-%m-%d)"
+
+## Clean up files older than 2 weeks. Create a new backup.
+find $backupPath -mtime +14 -type f -delete
+
+systemctl stop valheimserver.service
+
+#Wait for RAM to offload to .db and .db.old
+#allows for the closing of the databases.
+sleep 10
+
+## Tar Section. Create a backup file, with the current date in its name.
+## Add -h to convert the symbolic links into a regular files.
+## Backup some system files, also the entire `/home` directory, etc.
+## --exclude some directories, for example the the browser's cache, `.bash_history`, etc.
+tar zcvf "$backupPath/valheim-backup-$TODAY.tgz" /home/steam/.config/unity3d/IronGate/Valheim/worlds/*
+chown steam:steam $backupPath/valheim-backup-$TODAY.tgz
 echo "Copy backup to remote"
 rclone delete gdrive:valheim_backup/
 rclone copy $backupPath/$(ls -Art $backupPath | tail -n 1) gdrive:valheim_backup/
 rclone delete remote:/srv/data/valheim-backup/
 rclone copy $backupPath/$(ls -Art $backupPath | tail -n 1) remote:/srv/data/valheim-backup/
 echo "Done"
+
+systemctl start valheimserver.service
--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
-cd /opt/Dedicated_Valheim_Server_Script/
-./backup.sh
+/opt/valheim_tools/backup.sh
 #printf '1\n0\n' | ./menu.sh
 #printf '2\n7\ny\ny\n0\n0\n' | ./menu.sh
 #systemctl restart valheimserver.service
